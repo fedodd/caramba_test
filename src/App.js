7f5af029a61dd1a6697b112ed88e163edaefb3a5
fedodd <fedodd.s@gmail.com>
2020-07-28 11:48:26 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+class App extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div id="app">
+        <h1>Hello, world!</h1>
+      </div>
+    );
+  }
+}
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pcss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 8000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: require('html-webpack-template'),
+      title: 'Ya caramba!',
+      inject: false,
+      appMountId: 'app',
+      filename: 'index.html',
+      links: [
+        'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700;900&display=swap',
+      ],
+      meta: {
+        viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      },
+    }),
+  ],
+};
+
+module.exports = config;
