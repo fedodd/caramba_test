906fa2c9fc608cb1aa2ca2f76e272aaa2b560697
fedodd <fedodd.s@gmail.com>
2020-08-04 18:45:38 +0300

    start table style

--- src/containers/table/tableRow.js
+++ src/containers/table/tableRow.js
@@ -1,14 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import classes from "./table.pcss";
 
 const tableRow = (props) => {
+  const colorStyle =
+    props.car.color === "white"
+      ? {
+          backgroundColor: `var(--color-car_${props.car.color})`,
+          border: "1px solid #DDDDDD",
+        }
+      : { backgroundColor: `var(--color-car_${props.car.color})` };
+
+  console.log(colorStyle);
+
   return (
     <tr>
       <td>
         {props.car.title}
-        <p>{props.car.description}</p>
+        <p className={classes.descr}>{props.car.description}</p>
       </td>
       <td>{props.car.year}</td>
-      <td>{props.car.color}</td>
+      <td>
+        <div className={classes.color} style={colorStyle}></div>
+      </td>
       <td>{props.car.status}</td>
       <td>
         {props.car.price} руб. <button>Удалить</button>
